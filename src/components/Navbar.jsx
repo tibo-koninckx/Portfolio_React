31fa9a6883b105d284c4fa28483bb3ac9adc0a82
tibo-koninckx <r0929614@student.thomasmore.be>
2023-12-17 17:18:04 +0100

    add Navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,54 @@
+import {useState} from "react";
+import {AppBar, Drawer, IconButton, List, ListItem, Toolbar} from "@mui/material";
+import {Link} from "react-router-dom";
+import MenuIcon from '@mui/icons-material/Menu';
+
+export function Navbar() {
+    const [drawerOpen, setDrawerOpen] = useState(false);
+
+    const handleDrawerOpen = () => {
+        setDrawerOpen(true);
+    };
+
+    const handleDrawerClose = () => {
+        setDrawerOpen(false);
+    };
+    return <>
+
+        <div>
+            <AppBar position="static">
+                <Toolbar>
+                    <IconButton
+                        edge="start"
+                        color="inherit"
+                        aria-label="menu"
+                        onClick={handleDrawerOpen}
+                        sx={{mr: 2, display: {xs: 'block', md: 'none'}}}
+                    >
+                        <MenuIcon/>
+                    </IconButton>
+                    <Link to="/" style={{textDecoration: 'none', color: 'inherit'}}>
+                        <div>Logo</div>
+                    </Link>
+                    <div style={{flexGrow: 1}}></div>
+                    {/* Add this line to push menu items to the right */}
+                    <div className="desktop-menu" style={{display: {xs: 'none', md: 'flex'}}}>
+                        <Link to="/" style={{textDecoration: 'none', color: 'inherit', margin: '0 10px'}}>
+                            Home
+                        </Link>
+                    </div>
+                </Toolbar>
+            </AppBar>
+
+            <Drawer anchor="left" open={drawerOpen} onClose={handleDrawerClose}>
+                <List>
+                    <ListItem onClick={handleDrawerClose}>
+                        <Link to="/" style={{textDecoration: 'none', color: 'inherit'}}>
+                            Home
+                        </Link>
+                    </ListItem>
+                </List>
+            </Drawer>
+        </div>
+    </>
+}
