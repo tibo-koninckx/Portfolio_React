40c429132ebaaa277dac88514b9437d79b7feba2
tibo-koninckx <r0929614@student.thomasmore.be>
2023-12-17 17:19:11 +0100

    add navbar and import react

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
+import React from 'react';
 import './App.css';
 import {HomePage} from "./pages/HomePage";
 import {BrowserRouter, Route, Routes} from "react-router-dom";
+import {Navbar} from "./components/Navbar";
+
 
 function App() {
     return <>
@@ -11,6 +14,7 @@ function App() {
 function ProvidedApp() {
     return <>
         <BrowserRouter>
+            <Navbar/>
             <Routes>
                 <Route index path={"/"} element={<HomePage/>}/>
             </Routes>
