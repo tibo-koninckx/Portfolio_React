efe1ebb967cab8997a4671b7f7e018ab1a2291e9
tibo-koninckx <r0929614@student.thomasmore.be>
2023-12-17 17:47:18 +0100

    add H1 to Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,8 @@ export function Header() {
         <Grid container spacing={2}>
             <Grid item xs={12} sm={10} md={8} lg={8}>
                 <Container maxWidth="md">
-                    <Typography  variant="h4" sx={{fontFamily: 'Nunito, sans-serif', marginTop: '35%', mx: 2}}>I'AM A WEB DEVELOPER, I TRANSFORM YOUR
+                    <Typography variant="h1" sx={{fontFamily: 'Nunito, sans-serif', marginTop: '5%', mx: 2}}>Tibo Koninckx</Typography>
+                    <Typography  variant="h4" sx={{fontFamily: 'Nunito, sans-serif', marginTop: '15%', mx: 2}}>I'AM A WEB DEVELOPER, I TRANSFORM YOUR
                         DESIRES INTO VIBRANT CODE, BRINGING YOUR IDEAS TO LIFE ON THE DIGITAL CANVAS OF WEBSITES</Typography>
                     <Typography variant="subtitle1" mt={2} sx={{fontFamily: 'Nunito, sans-serif', mx:2}}>Passionate about web development and currently pursuing a
                         degree in programming at Thomas More University of Applied Sciences. Transforming ideas into code and embracing the journey of continuous
