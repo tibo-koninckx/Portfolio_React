969a36bfe26efa691072722fdc9c8a0b87aa4e14
tibo-koninckx <r0929614@student.thomasmore.be>
2023-12-17 17:19:36 +0100

    change responsive size and typography variant

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,9 +3,9 @@ import {Container, Grid, Typography} from "@mui/material";
 export function Header() {
     return <>
         <Grid container spacing={2}>
-            <Grid item xs={12} sm={12} md={12} lg={8}>
+            <Grid item xs={12} sm={10} md={8} lg={8}>
                 <Container maxWidth="md">
-                    <Typography  variant="h3" sx={{fontFamily: 'Nunito, sans-serif', marginTop: '35%', mx: 2}}>I'AM A WEB DEVELOPER, I TRANSFORM YOUR
+                    <Typography  variant="h4" sx={{fontFamily: 'Nunito, sans-serif', marginTop: '35%', mx: 2}}>I'AM A WEB DEVELOPER, I TRANSFORM YOUR
                         DESIRES INTO VIBRANT CODE, BRINGING YOUR IDEAS TO LIFE ON THE DIGITAL CANVAS OF WEBSITES</Typography>
                     <Typography variant="subtitle1" mt={2} sx={{fontFamily: 'Nunito, sans-serif', mx:2}}>Passionate about web development and currently pursuing a
                         degree in programming at Thomas More University of Applied Sciences. Transforming ideas into code and embracing the journey of continuous
