b11f5d023ad32def450f873acc47c18d57e51ca7
tibo-koninckx <r0929614@student.thomasmore.be>
2023-12-26 13:02:43 +0100

    add h2 title

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,7 +1,12 @@
-import {Grid} from "@mui/material";
+import {Grid, Typography} from "@mui/material";
 
 export function About() {
     return <>
-        <Grid container spacing={2}></Grid>
+        <Grid container spacing={2}>
+            <Grid xs={12} sm={10} lg={12}>
+                <Typography variant="h2" sx={{fontFamily: 'Nunito, sans-serif', marginTop: '5%', textAlign: 'center'}}>About me</Typography>
+
+            </Grid>
+        </Grid>
     </>
 }
