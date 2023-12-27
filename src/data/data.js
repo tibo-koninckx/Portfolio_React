8ddd92454dce4f59186e1b5c8f31315daf9211b3
tibo-koninckx <r0929614@student.thomasmore.be>
2023-12-27 13:53:56 +0100

    add bioGraphy to data fille

--- src/data/data.js
+++ src/data/data.js
@@ -21,3 +21,9 @@ export const Tools = [
     {name: "Visual studio code"},
     {name: "office"}
 ]
+
+export const Biogrphy = [
+    "My name is Tibo Koninckx, I live in Mortsel. I completed a degree in programming at Thomas More University of " +
+    "Applied Sciences in Sint Katelijne Waver (De Nayer campus). I became interested in Web development when a secondary school teacher " +
+    "explained how to create a website with HTML and CSS. That's how I became interested in web development."
+]
