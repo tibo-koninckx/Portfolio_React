4a1d72ec00d347ecd30644433d49fa08c4632a48
tibo-koninckx <r0929614@student.thomasmore.be>
2023-12-27 13:33:34 +0100

    add data in arrays

---
+++ src/data/data.js
@@ -0,0 +1,24 @@
+export const FullStack = [
+    {name: "HTML"},
+    {name: "CSS"},
+    {name: "Javascript & React"},
+    {name: "Java spring Boot"},
+    {name: "Databanken: SQL (Postgres, Mysql)\n" +
+            "Nosql (MongoDb, Firbase)"},
+    {name: "git, gitHub"},
+    {name: "HTML"},
+    {name: "Test Driven"},
+    {name: "Unit Testing"},
+];
+
+export const Frameworks = [
+    {name: "Material UI"},
+    {name: "Ant Design"}
+];
+
+export const Tools = [
+    {name: "intelij"},
+    {name: "Visual studio"},
+    {name: "Visual studio code"},
+    {name: "office"}
+]
