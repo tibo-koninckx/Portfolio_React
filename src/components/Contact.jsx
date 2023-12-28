29bc3d422552175b23d55936e4b6c29370eee832
tibo-koninckx <r0929614@student.thomasmore.be>
2023-12-28 12:31:32 +0100

    add flexbox allignment center of page

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,7 +2,12 @@ import {Box, Grid, TextField, Typography} from "@mui/material";
 
 export function Contact() {
     return <>
-        <Box component="form" autoComplete="off" sx={{textAlign: 'center', background: 'white'}}>
+        <Box component="form" autoComplete="off" sx={{
+            display: 'flex',
+            justifyContent: 'center',
+            background: 'white',
+            '& .MuiTextField-root': { m: 1, width: '50ch' }
+        }}>
             <Grid>
                 <Grid item xs={12} sm={6} md={6} lg={6}>
                     <TextField sx={{mr: 2}}
