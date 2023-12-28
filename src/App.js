78ed6560cc9e13fc8fbebb908ca5ad996d0a8105
tibo-koninckx <r0929614@student.thomasmore.be>
2023-12-28 11:44:02 +0100

    add route for contact Page

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import {BrowserRouter, Route, Routes} from "react-router-dom";
 import {Navbar} from "./components/Navbar";
 import {Biogrphy, Frameworks, FullStack, Interests, Tools} from "./data/data";
 import {AboutPage} from "./pages/AboutPage";
+import {ContactPage} from "./pages/ContactPage";
 
 
 function App() {
@@ -20,6 +21,7 @@ function ProvidedApp() {
             <Routes>
                 <Route index path={"/"} element={<HomePage/>}/>
                 <Route path={"/about"} element={<AboutPage FullStack={FullStack} Frameworks={Frameworks} Tools={Tools} Bio={Biogrphy} Interests={Interests}/>}/>
+                <Route path={"/contact"} element={<ContactPage/>}/>
             </Routes>
         </BrowserRouter>
     </>
