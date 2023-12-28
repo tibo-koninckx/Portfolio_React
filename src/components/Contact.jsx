44f1caec526e04e073bf65ecdea390dc2fd9e356
tibo-koninckx <r0929614@student.thomasmore.be>
2023-12-28 13:24:33 +0100

    add div

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -3,49 +3,50 @@ import {Box, Grid, TextField, Typography} from "@mui/material";
 export function Contact() {
     return <>
         <Box component="form" autoComplete="off" sx={{
-            display: 'flex',
-            justifyContent: 'center',
             background: 'white',
-            '& .MuiTextField-root': { m: 1, width: '50ch' }
+            height: '100vh',
+            '& .MuiTextField-root': { m: 1, width: '50ch', }
         }}>
-            <Grid>
-                <Grid item xs={12} sm={6} md={6} lg={6}>
-                    <TextField sx={{mr: 2}}
-                        id="FirstName"
-                        label="FirstName"
-                        variant="outlined"
-                        margin="normal"
-                        required
-                    />
+            <div className="contact ">
+                <Grid>
+                    <Grid item xs={12} sm={6} md={8} lg={12}>
+                        <TextField sx={{mr: 2}}
+                                   id="FirstName"
+                                   label="FirstName"
+                                   variant="outlined"
+                                   margin="normal"
+                                   required
+                        />
 
-                    <TextField
-                               id="LastName"
-                               label="LastName"
-                               variant="outlined"
-                               margin="normal"
-                               required
-                    />
+                        <TextField
+                            id="LastName"
+                            label="LastName"
+                            variant="outlined"
+                            margin="normal"
+                            required
+                        />
+                    </Grid>
+                    <Grid item xs={12} sm={6} md={8} lg={12}>
+                        <TextField
+                            id="email"
+                            label="email"
+                            variant="outlined"
+                            margin="normal"
+                            required
+                        />
+                    </Grid>
+                    <Grid item xs={12} sm={12} md={12} lg={12}>
+                        <TextField
+                            id="message"
+                            label="Message"
+                            variant="outlined"
+                            margin="normal"
+                            multiline
+                            rows={4}
+                        />
+                    </Grid>
                 </Grid>
-                <Grid item xs={12} sm={6} md={6} lg={6}>
-                    <TextField
-                        id="email"
-                        label="email"
-                        variant="outlined"
-                        margin="normal"
-                        required
-                    />
-                </Grid>
-                <Grid item xs={12} sm={6} md={6} lg={6}>
-                    <TextField
-                        id="message"
-                        label="Message"
-                        variant="outlined"
-                        margin="normal"
-                        multiline
-                        rows={4}
-                    />
-                </Grid>
-            </Grid>
+            </div>
         </Box>
     </>
 }
