60aff69309f0635e9419463d80bf4a56f34375f1
tibo-koninckx <r0929614@student.thomasmore.be>
2023-12-28 12:03:52 +0100

    add Grid and form items

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,3 +1,19 @@
-export function Contact() {
+import {Box, Grid, TextField, Typography} from "@mui/material";
 
+export function Contact() {
+    return <>
+        <Box component="form" autoComplete="off" sx={{textAlign: 'center', background: 'white'}}>
+            <Grid>
+                <Grid item xs={12} sm={6} md={6} lg={6}>
+                    <TextField
+                        id="FirstName"
+                        label="FirstName"
+                        variant="outlined"
+                        margin="normal"
+                        required
+                    />
+                </Grid>
+            </Grid>
+        </Box>
+    </>
 }
