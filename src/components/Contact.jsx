17c0687c8717b4934e80b608f117a790f20eb280
tibo-koninckx <r0929614@student.thomasmore.be>
2023-12-28 12:05:42 +0100

    add lastName

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -5,12 +5,20 @@ export function Contact() {
         <Box component="form" autoComplete="off" sx={{textAlign: 'center', background: 'white'}}>
             <Grid>
                 <Grid item xs={12} sm={6} md={6} lg={6}>
-                    <TextField
+                    <TextField sx={{mr: 2}}
                         id="FirstName"
                         label="FirstName"
                         variant="outlined"
                         margin="normal"
                         required
+                    />
+
+                    <TextField
+                               id="LastName"
+                               label="LastName"
+                               variant="outlined"
+                               margin="normal"
+                               required
                     />
                 </Grid>
             </Grid>
