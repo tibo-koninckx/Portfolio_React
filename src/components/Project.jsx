5a46f15c3f630a5c4af8d898dd7de90303555224
tibo-koninckx <r0929614@student.thomasmore.be>
2023-12-30 12:02:11 +0100

    add Grid

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,3 +1,11 @@
-export function Project() {
+import {Grid} from "@mui/material";
 
+export function Project() {
+    return <>
+        <Grid>
+            <Grid item xs={12} sm={12} md={12} lg={12}>
+
+            </Grid>
+        </Grid>
+    </>
 }
