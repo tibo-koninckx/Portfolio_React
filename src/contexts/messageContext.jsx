6c32372cf49d56add69fd6b61b5e5837498722c5
tibo-koninckx <r0929614@student.thomasmore.be>
2023-12-30 11:36:23 +0100

    add useCallback clearMessage

--- src/contexts/messageContext.jsx
+++ src/contexts/messageContext.jsx
@@ -1,8 +1,12 @@
-import {createContext, useContext} from "react";
+import {createContext, useCallback, useContext, useState} from "react";
 
 const MessageContext = createContext();
 export function MessageProvoider() {
+    const [message, setMessage] = useState("");
 
+    const clearMessage = useCallback(() => {
+        setMessage("");
+    }, [message]);
 }
 
 export const useMessageContext = () => useContext(MessageContext);
