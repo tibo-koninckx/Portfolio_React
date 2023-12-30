3ff92f62eb73984abe65e149d12bfc9c0e356032
tibo-koninckx <r0929614@student.thomasmore.be>
2023-12-30 11:57:06 +0100

    add project route

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import {Biogrphy, Frameworks, FullStack, Interests, Tools} from "./data/data";
 import {AboutPage} from "./pages/AboutPage";
 import {ContactPage} from "./pages/ContactPage";
 import {MessageProvoider} from "./contexts/messageContext";
+import {ProjectPage} from "./pages/ProjectPage";
 
 
 function App() {
@@ -25,6 +26,7 @@ function ProvidedApp() {
                 <Route index path={"/"} element={<HomePage/>}/>
                 <Route path={"/about"} element={<AboutPage FullStack={FullStack} Frameworks={Frameworks} Tools={Tools} Bio={Biogrphy} Interests={Interests}/>}/>
                 <Route path={"/contact"} element={<ContactPage/>}/>
+                <Route path={"/projects"} element={<ProjectPage/>}/>
             </Routes>
         </BrowserRouter>
     </>
