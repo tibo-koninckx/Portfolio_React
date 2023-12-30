f3ee7e61c74f824f46ff721b1d8cedb0b0cabb75
tibo-koninckx <r0929614@student.thomasmore.be>
2023-12-30 12:06:19 +0100

    add Gcard from documentation

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,10 +1,29 @@
-import {Grid} from "@mui/material";
+import {Button, Card, CardActions, CardContent, CardMedia, Grid, Typography} from "@mui/material";
 
 export function Project() {
     return <>
         <Grid>
             <Grid item xs={12} sm={12} md={12} lg={12}>
-
+                <Card sx={{ maxWidth: 345 }}>
+                    <CardMedia
+                        sx={{ height: 140 }}
+                        image="/static/images/cards/contemplative-reptile.jpg"
+                        title="green iguana"
+                    />
+                    <CardContent>
+                        <Typography gutterBottom variant="h5" component="div">
+                            Lizard
+                        </Typography>
+                        <Typography variant="body2" color="text.secondary">
+                            Lizards are a widespread group of squamate reptiles, with over 6,000
+                            species, ranging across all continents except Antarctica
+                        </Typography>
+                    </CardContent>
+                    <CardActions>
+                        <Button size="small">Share</Button>
+                        <Button size="small">Learn More</Button>
+                    </CardActions>
+                </Card>
             </Grid>
         </Grid>
     </>
