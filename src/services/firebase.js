2f0508b7df2ffb0c7c5671902725c770796c3d70
tibo-koninckx <r0929614@student.thomasmore.be>
2024-01-05 10:56:50 +0100

    add storage bucket env link

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -1,6 +1,7 @@
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET
 };
 
 const app = initializeApp(firebaseConfig);
