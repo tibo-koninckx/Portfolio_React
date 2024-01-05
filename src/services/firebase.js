a9db377df251151c2cd522a0ea3c9e079505cee8
tibo-koninckx <r0929614@student.thomasmore.be>
2024-01-05 11:00:01 +0100

    add storagebucket to firebase

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -1,7 +1,10 @@
 import { initializeApp } from "firebase/app";
+import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
     storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET
 };
 
 const app = initializeApp(firebaseConfig);
+
+const storage = getStorage(app);
