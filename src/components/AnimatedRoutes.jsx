91cf390f798a66fc3c94d05c55db7c5242c6f3cc
tibo-koninckx <r0929614@student.thomasmore.be>
2024-01-13 14:39:16 +0100

    change all routes to AnimatedRoutes component

---
+++ src/components/AnimatedRoutes.jsx
@@ -0,0 +1,20 @@
+import {Route, Routes, useLocation} from "react-router-dom";
+import {HomePage} from "../pages/HomePage";
+import {AboutPage} from "../pages/AboutPage";
+import {Biogrphy, Frameworks, FullStack, Interests, Projects, Tools} from "../data/data";
+import {ContactPage} from "../pages/ContactPage";
+import {ProjectPage} from "../pages/ProjectPage";
+import React from "react";
+
+export function AnimatedRoutes() {
+    const location = useLocation();
+    return <>
+        <Routes location={location} key={location.pathname}>
+            <Route index path={"/"} element={<HomePage/>}/>
+            <Route path={"/about"} element={<AboutPage fullStack={FullStack} frameworks={Frameworks} tools={Tools} bio={Biogrphy} interests={Interests}/>}/>
+            <Route path={"/contact"} element={<ContactPage/>}/>
+            <Route path={"/projects"} element={<ProjectPage projects={Projects}/>}/>
+            <Route path={"*"} element={<HomePage/>}/>
+        </Routes>
+    </>
+}
