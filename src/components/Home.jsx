3918784857ff495c065bb935ca9344e068e701cc
tibo-koninckx <r0929614@student.thomasmore.be>
2024-01-13 13:30:48 +0100

    add motion to icons

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,4 +1,7 @@
-import {Container, Grid, Typography} from "@mui/material";
+import {Box, Container, Grid, Link, Typography} from "@mui/material";
+import {motion} from "framer-motion"
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import GitHubIcon from "@mui/icons-material/GitHub";
 
 export function Home() {
     return <>
@@ -12,6 +15,26 @@ export function Home() {
                         marginTop: {xl: '5%', lg: '5%', md: '5%', xs: '15%'},
                         mx: 2
                     }}>Full stack Web developer</Typography>
+                    <Typography variant="h3" sx={{
+                        fontFamily: 'Nunito, sans-serif',
+                        mt: 5,
+                        mx: 2
+                    }}><a style={{color:'white', textDecoration: 'none'}} href="mailto:tibo.koninckx@gmail.com">tibo.koninckx@gmail.com</a></Typography>
+                    <Typography variant="h3" sx={{
+                        fontFamily: 'Nunito, sans-serif',
+                        mt:2,
+                        mx: 2
+                    }}>+32 486 54 45 01</Typography>
+                    <Box sx={{ display: 'flex', justifyContent: 'center' }}>
+                        <motion.div whileHover={{scale: 1.5}}>
+                            <Link href="https://www.linkedin.com/in/tibokoninckx" target="_blank"><LinkedInIcon
+                                color="info" fontSize="large" sx={{mr: 2}}/></Link>
+                        </motion.div>
+                        <motion.div whileHover={{scale: 1.5}}>
+                            <Link href="https://github.com/tibo-koninckx" target="_blank"><GitHubIcon color="success"
+                                                                                                      fontSize="large"/></Link>
+                        </motion.div>
+                    </Box>
                 </Container>
             </Grid>
         </Grid>
