9ba1b970e316dd534c1f32881fd1824422d7d32f
tibo-koninckx <r0929614@student.thomasmore.be>
2024-01-13 15:11:59 +0100

    refactor code

--- src/App.js
+++ src/App.js
@@ -1,19 +1,15 @@
 import React from 'react';
 import './App.css';
-import {HomePage} from "./pages/HomePage";
-import {BrowserRouter, Route, Routes} from "react-router-dom";
+import {BrowserRouter} from "react-router-dom";
 import {Navbar} from "./components/Navbar";
-import {Biogrphy, Frameworks, FullStack, Interests, Projects, Tools} from "./data/data";
-import {AboutPage} from "./pages/AboutPage";
-import {ContactPage} from "./pages/ContactPage";
 import {MessageProvoider} from "./contexts/messageContext";
-import {ProjectPage} from "./pages/ProjectPage";
+import {AnimatedRoutes} from "./components/AnimatedRoutes";
 
 
 function App() {
     return <>
         <MessageProvoider>
-        <ProvidedApp/>
+            <ProvidedApp/>
         </MessageProvoider>
     </>
 }
@@ -22,13 +18,7 @@ function ProvidedApp() {
     return <>
         <BrowserRouter>
             <Navbar/>
-            <Routes>
-                <Route index path={"/"} element={<HomePage/>}/>
-                <Route path={"/about"} element={<AboutPage fullStack={FullStack} frameworks={Frameworks} tools={Tools} bio={Biogrphy} interests={Interests}/>}/>
-                <Route path={"/contact"} element={<ContactPage/>}/>
-                <Route path={"/projects"} element={<ProjectPage projects={Projects}/>}/>
-                <Route path={"*"} element={<HomePage/>}/>
-            </Routes>
+            <AnimatedRoutes/>
         </BrowserRouter>
     </>
 }
