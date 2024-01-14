4e060e48254bf903807067c7f5850c9b5503c5b1
tibo-koninckx <r0929614@student.thomasmore.be>
2024-01-14 14:26:29 +0100

    set duration to 2

--- src/components/PageAnimation.jsx
+++ src/components/PageAnimation.jsx
@@ -2,10 +2,10 @@ import {motion} from "framer-motion"
 
 export function PageAnimation(props) {
     const {children} = props;
-    return<>
-        <motion.div  animate={{ height: "auto", y:0, transition: { duration: 3 } }}
-                     initial={{ height: 0, y:'100%'}}
-                     style={{ overflow: "hidden", position: 'relative'}}>
+    return <>
+        <motion.div animate={{height: "auto", x: 0, width: "100%", transition: {duration: 2}}}
+                    initial={{height: 0, x: '-100%', width: "100%"}}
+                    style={{overflow: "hidden", position: 'relative'}}>
             {children}
         </motion.div>
     </>
