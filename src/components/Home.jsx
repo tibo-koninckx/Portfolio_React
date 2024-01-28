21a05bd238970c5200141658c3f5b13510747a26
tibo-koninckx <r0929614@student.thomasmore.be>
2024-01-28 11:11:14 +0100

    add fontSize and cursor pointer

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -10,24 +10,27 @@ export function Home() {
                     <Container style={{textAlign: 'center'}}>
                         <Typography variant="h1" sx={{fontFamily: 'Nunito, sans-serif', marginTop: '5%', mx: 2}}>Tibo
                             Koninckx</Typography>
-                        <Typography variant="h3" sx={{
+                        <Typography variant="body1" sx={{
                             fontFamily: 'Nunito, sans-serif',
+                            fontSize: 40,
                             marginTop: {xl: '5%', lg: '5%', md: '5%', xs: '15%'},
                             mx: 2
                         }}>Full stack Web developer</Typography>
-                        <Typography variant="h3" sx={{
+                        <Typography className="no-first-letter" variant="body1" sx={{
+                            fontSize:40,
                             mt: 5,
-                        }}><a style={{color:'white', textDecoration: 'none'}} href="mailto:tibo.koninckx@gmail.com">tibo.koninckx@gmail.com</a></Typography>
-                        <Typography variant="h3" sx={{
+                        }}><a style={{color: 'white', textDecoration: 'none'}} href="mailto:tibo.koninckx@gmail.com">tibo.koninckx@gmail.com</a></Typography>
+                        <Typography className="no-first-letter" variant="body1" sx={{
                             fontFamily: 'Nunito, sans-serif',
+                            fontSize: 40,
                             mt:2,
                             mx: 2
                         }}>+32 486 54 45 01</Typography>
                         <Box sx={{ display: 'flex', justifyContent: 'center' }}>
-                            <motion.div whileHover={{scale: 1.2}}>
+                            <motion.div whileHover={{scale: 1.2, cursor: 'pointer'}}>
                                 <Link className="no-style" to="https://www.linkedin.com/in/tibokoninckx" target="_blank"><FontAwesomeIcon icon={faLinkedin} size="2xl"/></Link>
                             </motion.div>
-                            <motion.div whileHover={{scale: 1.2}}>
+                            <motion.div whileHover={{scale: 1.2, cursor: 'pointer'}}>
                                 <Link className="no-style" to="https://github.com/tibo-koninckx" target="_blank"><FontAwesomeIcon style={{marginLeft: '50%'}} icon={faGithub} size="2xl" /></Link>
                             </motion.div>
                         </Box>
