1ed63e4ea2034527f74d55ef3d252329ef851688
tibo-koninckx <r0929614@student.thomasmore.be>
2024-02-01 12:13:00 +0100

    create logo component

---
+++ src/components/Logo.jsx
@@ -0,0 +1,12 @@
+import {Box, Typography} from "@mui/material";
+
+export function Logo(props) {
+    const {children, name} = props;
+
+    return <Box style={{fontSize: '5em'}} sx={{px:2}}>
+        {children}
+        <Typography variant="body1"
+                    sx={{fontFamily: 'Nunito, sans-serif', textAlign: 'center', fontSize: 20}}>{name}
+        </Typography>
+    </Box>
+}
