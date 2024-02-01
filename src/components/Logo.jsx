3209356d5d8a67721f9421ecb855d9f88c3b071e
tibo-koninckx <r0929614@student.thomasmore.be>
2024-02-01 13:25:51 +0100

    add whileHover

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,10 +1,11 @@
 import {Box, Typography} from "@mui/material";
+import {motion} from "framer-motion"
 
 export function Logo(props) {
     const {children, name} = props;
 
     return <Box style={{fontSize: '5em'}} sx={{px:2}}>
-        {children}
+        <motion.div whileHover={{scale: 0.8, backgroundColor: '#666666', }}>{children}</motion.div>
         <Typography variant="body1"
                     sx={{fontFamily: 'Nunito, sans-serif', textAlign: 'center', fontSize: 20}} className="no-first-letter">{name}
         </Typography>
