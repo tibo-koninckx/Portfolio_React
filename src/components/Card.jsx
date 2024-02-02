bdc3199c63c8660b05989973c2bd55e567ddbf42
tibo-koninckx <r0929614@student.thomasmore.be>
2024-02-02 13:18:59 +0100

    add margin and padding to card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,16 +1,16 @@
 import {Box, Button, Container, Typography} from "@mui/material";
 
 export function Card(props) {
-    const {title, description, url, buttonName} = props;
-    return <Box sx={{background: '#555555'}}>
+    const {title, description, url} = props;
+    return <Box sx={{background: '#555555', mt:'5%', mx:'2%', pt:'1%', pb:'1%', maxWidth: '20%'}}>
         <Container>
             <Typography sx={{fontFamily: 'Nunito, sans-serif'}} gutterBottom variant="h5" component="div">
                 {title}
             </Typography>
-            <Typography sx={{fontFamily: 'Nunito, sans-serif'}} variant="body1" color="text.secondary">
+            <Typography sx={{fontFamily: 'Nunito, sans-serif'}} variant="body1" color="white">
                 {description}
             </Typography>
-            <Button size="small" href={url} target="_blank">{buttonName}</Button>
+            <Button size="small" href={url} target="_blank">Read More</Button>
         </Container>
     </Box>
 }
