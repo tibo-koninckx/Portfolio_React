4648e7ba872b76864c77578cfe0bb4b0e6cdbc05
tibo-koninckx <r0929614@student.thomasmore.be>
2024-02-02 13:52:31 +0100

    add date

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,7 +1,7 @@
 import {Box, Button, Container, Typography} from "@mui/material";
 
 export function Card(props) {
-    const {title, description, url} = props;
+    const {title, description, url, date} = props;
     return <Box sx={{background: '#555555', mt:'5%', mx:'2%', pt:'1%', pb:'1%', maxWidth: '20%'}}>
         <Container>
             <Typography sx={{fontFamily: 'Nunito, sans-serif'}} gutterBottom variant="h5" component="div">
