cedb481793c6fd1402f3214328f4bfefb43b3c30
tibo-koninckx <r0929614@student.thomasmore.be>
2024-02-02 13:52:04 +0100

    delete ExtractFrontmatter replaced by gray matter library

--- src/helpers/ConvertMdToJs.js
+++ src/helpers/ConvertMdToJs.js
@@ -1,18 +1,32 @@
 import React, { useEffect, useState } from "react";
 import Markdown from "react-markdown";
 import { Box } from "@mui/material";
-import {ExtractFrontmatter} from "./ExtractFrontmatter";
+import matter from "gray-matter";
+import { Buffer } from 'buffer';
+global.Buffer = Buffer;
 
 export function ConvertMdToJs(props) {
     const { mdFilePath, className, display } = props;
-    const [readable, setReadable] = useState({ title: "", content: "" , description: "", date: ""});
+    const [readable, setReadable] = useState({
+        title: "",
+        content: "",
+        description: "",
+        date: "",
+    });
 
     useEffect(() => {
         fetch(mdFilePath)
             .then((res) => res.text())
             .then((md) => {
-                const { title, content, description, date } = ExtractFrontmatter(md);
-                setReadable({ title, content, description, date });
+                const { data, content } = matter(md);
+                const { title, description, date } = data;
+
+                setReadable({
+                    title: title || "",
+                    content: content || "",
+                    description: description || "",
+                    date: date || "",
+                });
             });
     }, [mdFilePath]);
 
@@ -30,7 +44,11 @@ export function ConvertMdToJs(props) {
                 <div className={`title ${className}`}>{readable.title}</div>
             )}
             {display === "content" && (
-                <Markdown className={`content ${className}`} children={readable.content} components={components} />
+                <Markdown
+                    className={`content ${className}`}
+                    children={readable.content}
+                    components={components}
+                />
             )}
             {display === "description" && (
                 <div className={`description ${className}`}>{readable.description}</div>
