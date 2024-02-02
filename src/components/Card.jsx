4d4995e561c8959a4316c8c88cf602e90b9fa421
tibo-koninckx <r0929614@student.thomasmore.be>
2024-02-02 11:05:24 +0100

    add extra settings to card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,8 +1,16 @@
-import {Box} from "@mui/material";
+import {Box, Button, Container, Typography} from "@mui/material";
 
 export function Card(props) {
-    const {title, description, url} = props;
+    const {title, description, url, buttonName} = props;
     return <Box sx={{background: '#555555'}}>
-
+        <Container>
+            <Typography sx={{fontFamily: 'Nunito, sans-serif'}} gutterBottom variant="h5" component="div">
+                {title}
+            </Typography>
+            <Typography sx={{fontFamily: 'Nunito, sans-serif'}} variant="body1" color="text.secondary">
+                {description}
+            </Typography>
+            <Button size="small" href={url} target="_blank">{buttonName}</Button>
+        </Container>
     </Box>
 }
