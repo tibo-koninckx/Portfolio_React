5ffcff66ddf1898f667a7c3706099d5e402439dd
tibo-koninckx <r0929614@student.thomasmore.be>
2024-02-02 11:00:12 +0100

    add props

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,3 +1,8 @@
-export function Card() {
+import {Box} from "@mui/material";
 
+export function Card(props) {
+    const {title, description, url} = props;
+    return <Box sx={{background: '#555555'}}>
+
+    </Box>
 }
