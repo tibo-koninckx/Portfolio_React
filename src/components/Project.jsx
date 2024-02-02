2f87c3f36e9e74c020fba014d0b906cca3f9c141
tibo-koninckx <r0929614@student.thomasmore.be>
2024-02-02 16:41:29 +0100

    get all markdown filles and loop

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,31 +1,24 @@
-import {Button, Card, CardActions, CardContent, CardMedia, Grid, Typography} from "@mui/material";
+import {Grid} from "@mui/material";
+import {Card} from "./Card";
+import {Buffer} from 'buffer';
+import {ConvertMdToJs} from "../helpers/ConvertMdToJs";
+import {FlexContainer} from "./FlexContainer";
 
-export function Project(props) {
-    const {projects} = props;
-    return <>
-        <Grid container>
-                {projects.map((p, index) => (
-                    <Grid container justifyContent="center" item xs={12} sm={6} md={4} lg={3} key={index}>
-                    <Card sx={{maxWidth: 345, mt:5, mx:{sm:2, md:2, lg:2}}}>
-                        {p.image? <CardMedia
-                            sx={{ height: 140 }}
-                            image={p.image}
-                            title={p.imageTitle}
-                        />:''}
-                        <CardContent>
-                            <Typography sx={{fontFamily: 'Nunito, sans-serif'}} gutterBottom variant="h5" component="div">
-                                {p.title}
-                            </Typography>
-                            <Typography sx={{fontFamily: 'Nunito, sans-serif'}} variant="body1" color="text.secondary">
-                                {p.description}
-                            </Typography>
-                        </CardContent>
-                        <CardActions>
-                            <Button size="small" href={p.github} target="_blank">Learn More</Button>
-                        </CardActions>
-                    </Card>
-                    </Grid>
+global.Buffer = Buffer;
+
+
+export function Project() {
+    const context = require.context('../content/projects', false, /\.md$/);
+    const markdownFiles = context.keys().map(context);
+    return (
+        <>
+            <Grid justifyContent="center" item xs={12} sm={6} md={4} lg={3}>
+                <FlexContainer>
+                    {markdownFiles.map((project, index) => (
+                        <Card key={index} title={<ConvertMdToJs mdFilePath={project} display="title"/>}/>
                     ))}
-        </Grid>
-    </>
+                </FlexContainer>
+            </Grid>
+        </>
+    );
 }
