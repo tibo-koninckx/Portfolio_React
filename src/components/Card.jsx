3b90131d2d1dc9f7cc98ff70da395abcf49dc181
tibo-koninckx <r0929614@student.thomasmore.be>
2024-02-05 12:04:28 +0100

    add Fillepath to link

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -4,7 +4,7 @@ import {faArrowRightLong} from "@fortawesome/free-solid-svg-icons"
 import {Link} from "react-router-dom";
 
 export function Card(props) {
-    const {title, description, url, date} = props;
+    const {title, description, url, date, fillepath} = props;
     return <Box sx={{background: '#555555', mt:'5%', mx:'2%', pt:'1%', pb:'1%', maxWidth: '20%', minWidth: '20%'}}>
         <Container>
             <Typography sx={{fontFamily: 'Nunito, sans-serif', fontWeight: 'bold'}} gutterBottom variant="subtitle2" component="div">
@@ -16,7 +16,7 @@ export function Card(props) {
             <Typography className="no-first-letter" sx={{fontFamily: 'Nunito, sans-serif'}} variant="body1" color="white">
                 {description}
             </Typography>
-            <Link to={`/projects/${url}`}>
+            <Link to={`/projects/${url}`} state={fillepath}>
                 <Button sx={{color: 'white'}} size="small" target="_blank">Read More <FontAwesomeIcon icon={faArrowRightLong} /></Button>
             </Link>
         </Container>
