8e366a4c1d49f5500e64cfc405dc5d72ed11a326
tibo-koninckx <r0929614@student.thomasmore.be>
2024-02-05 10:40:51 +0100

    use FilleName as filelUrl

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,22 +1,22 @@
 import {Grid} from "@mui/material";
 import {Card} from "./Card";
-import {Buffer} from 'buffer';
 import {ConvertMdToJs} from "../helpers/ConvertMdToJs";
 import {FlexContainer} from "./FlexContainer";
-
-global.Buffer = Buffer;
-
+import matter from "gray-matter";
 
 export function Project() {
     const context = require.context('../content/projects', false, /\.md$/);
     const markdownFiles = context.keys().map(context);
+    const filleUrl = context.keys().map(p => p.replace(".md", '').toLowerCase());
     return (
         <>
             <Grid justifyContent="center" item xs={12} sm={6} md={4} lg={3}>
                 <FlexContainer>
-                    {markdownFiles.map((project, index) => (
-                        <Card key={index} title={<ConvertMdToJs mdFilePath={project} display="title"/>}/>
-                    ))}
+                    {markdownFiles.map((project, index) => {
+                        return (
+                            <Card key={index} title={<ConvertMdToJs mdFilePath={project} display="title" />} url={filleUrl[index]} />
+                        );
+                    })}
                 </FlexContainer>
             </Grid>
         </>
