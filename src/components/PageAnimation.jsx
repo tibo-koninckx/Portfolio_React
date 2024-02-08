59a60096117b2376343cded30a295164be57f13c
tibo-koninckx <r0929614@student.thomasmore.be>
2024-02-08 10:54:24 +0100

    use vh for y animation

--- src/components/PageAnimation.jsx
+++ src/components/PageAnimation.jsx
@@ -5,7 +5,7 @@ export function PageAnimation(props) {
     return <>
         <motion.div animate={{height: "auto", x: 0, width: "100%", transition: {duration: 2}}}
                     initial={{height: 0, x: '-100%', width: "100%"}}
-                    exit={{y: '100%', transition:{duration: 2}}}
+                    exit={{y: '100vh', transition:{duration: 2}}}
                     style={{overflow: "hidden", position: 'relative'}}>
             {children}
         </motion.div>
