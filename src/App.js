0c8f5aae7fe25d88944fb46e9390b04ed75540d9
tibo-koninckx <r0929614@student.thomasmore.be>
2024-02-08 11:08:55 +0100

    add particles settings

--- src/App.js
+++ src/App.js
@@ -7,6 +7,33 @@ import {AnimatedRoutes} from "./components/AnimatedRoutes";
 
 
 function App() {
+    // eslint-disable-next-line no-undef
+    tsParticles.load({
+        id: "tsparticles",
+        options: {
+            preset: "triangles",
+            background: {
+                opacity: 0
+            },
+            particles: {
+                move: {
+                    speed: 2
+                },
+            },
+            responsive: [
+                {
+                    options: {
+                        particles: {
+                            number: {
+                                value: 50
+                            },
+                        },
+                    },
+                    maxWidth: 800
+                },
+            ]
+        },
+    });
     return <>
         <MessageProvoider>
             <ProvidedApp/>
