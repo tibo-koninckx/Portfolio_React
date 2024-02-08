7bb800047b05b4f684797d9b8f2c865c6f2d67a1
tibo-koninckx <r0929614@student.thomasmore.be>
2024-02-08 11:34:39 +0100

    change min and max width

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -5,7 +5,7 @@ import {Link} from "react-router-dom";
 
 export function Card(props) {
     const {title, description, url, date, fillepath} = props;
-    return <Box sx={{background: '#555555', mt:'5%', mx:'2%', pt:'1%', pb:'1%', maxWidth: '20%', minWidth: '20%'}}>
+    return <Box sx={{background: '#555555', mt:'5%', mx:'2%', pt:'1%', pb:'1%', maxWidth: 'auto',  minWidth: '30%'}}>
         <Container>
             <Typography sx={{fontFamily: 'Nunito, sans-serif', fontWeight: 'bold'}} gutterBottom variant="subtitle2" component="div">
                 {date}
