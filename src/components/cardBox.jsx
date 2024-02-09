69ca3c7beaef5076ee85a7e5f5965169fc975b10
tibo-koninckx <r0929614@student.thomasmore.be>
2024-02-09 11:02:21 +0100

    create component cardBox

---
+++ src/components/cardBox.jsx
@@ -0,0 +1,10 @@
+import {Box} from "@mui/material";
+
+export function CardBox(props) {
+    const {children, style} = props;
+    return <>
+        <Box style={style} sx={{background: '#555555', marginLeft:{lg: '11%', xl:'11%', md:'11%'}, marginRight: {lg: '11%', xl:'11%', md:'11%'}, pb:2, pt:2}}>
+            {children}
+        </Box>
+    </>
+}
