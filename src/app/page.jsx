aac6932e2144a9170fdd36ca2b655c9af0d86865
tibo-koninckx <r0929614@student.thomasmore.be>
2024-03-02 14:23:57 +0100

    add pageAnimation

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,8 +1,11 @@
 'use client';
 import {Home} from "../components/Home";
+import {PageAnimation} from "../components/PageAnimation";
 
 export default function HomePage() {
     return <>
+        <PageAnimation>
         <Home/>
+        </PageAnimation>
     </>
 }
